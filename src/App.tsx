1145c0209b917841f04c9d5b1bb116bc1d39a208
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-11-16 21:39:59 +0530

    initial setup complete

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import  Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <main className="w-screen h-screen bg-black text-white"> 
+    <Navbar></Navbar>
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  return (
+    <nav className="w-screen h-20 px-10 py-8">
+      <ul className="text-3xl flex items-center gap-8">
+        <a className=""  href="/blogs" >
+          <span className="text-4xl font-bold">[</span>
+          <span className="font-semibold"> Blogs </span>
+          <span className="text-4xl font-bold">]</span>
+        </a>
+
+        <a >
+          <span className="text-4xl font-bold">[</span>
+          <span className="font-semibold"> Blogs </span>
+          <span className="text-4xl font-bold">]</span>
+        </a>
+
+        <a >
+          <span className="text-4xl font-bold">[</span>
+          <span className="font-semibold"> Blogs </span>
+          <span className="text-4xl font-bold">]</span>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
