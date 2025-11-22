525df77ef1d3915dd8d957d8b38fc7ec84d5b72d
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-11-22 22:53:59 +0530

    feat: enhance styling and layout of HomePage, Navbar, and global font settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,77 @@
 import Navbar from "./Components/Navbar"
 import HomePage from './Components/HomePage.tsx'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { animate, motion } from "motion/react"
 
 function App() {
     const imgRef = useRef(null)
+    const [banner_timer, setBanner_timer] = useState(0)
+
+    const banner_animation_time = 10
 
 
-    useEffect(() => {
-        const img = document.querySelectorAll(".banner-image")
-        console.log(document.querySelector(".banner-image"))
-        console.log(img)
-        if (img.length) {
-            // animate(".banner-image", { y: 0 }, { duration: 30, ease: "easeInOut" })
-        }
-
-    }, [])
+    // useEffect(() => {
+    //
+    //     const interval = setInterval((prevSecond: number) => {
+    //         console.log("interval ran")
+    //         animate(".banner-image", {
+    //             y: 0
+    //         }, {
+    //             duration: banner_animation_time,
+    //             repeat: Infinity,
+    //             ease: "easeInOut"
+    //         })
+    //
+    //     }, banner_animation_time * 1000)
+    //
+    //     return () => {
+    //         clearInterval(interval)
+    //     }
+    //
+    //     // const img = document.querySelectorAll(".banner-image")
+    //     // console.log(document.querySelector(".banner-image"))
+    //     // console.log(img)
+    //     // if (img.length) {
+    //     //     animate(".banner-image", { y: 0 }, { duration: 30, ease: "easeInOut" })
+    //     // }
+    // }, [])
 
     return (
         <main className="w-screen h-full bg-black text-white">
-            <div className="relative h-[14vw] flex items-start justify-center overflow-hidden">
+            <div className="relative h-10 lg:h-30 flex items-start justify-center overflow-hidden">
                 <motion.img
                     ref={imgRef}
                     className="banner-image bg-red-200 w-[80vw]"
                     src="/ascii-art.png"
-                    initial={{
-                        y: -300
-                    }}
-
+                    initial={{ y: "-50%" }}
+                    animate={{ y: "0%" }}
                     transition={{
-                        duration: 40,
-                        repeat: Infinity
-
+                        duration: 10,
+                        ease: "easeInOut",
+                        repeat: Infinity,
+                        repeatType: "mirror",
                     }}
+                // initial={{ y: "-50%" }}
+                // animate={{ y: ["-50%", "0%", "-50%"] }}   // <-- back & forth loop
+                // transition={{
+                //   duration: 8,        // total time for the full cycle
+                //   ease: "easeInOut",
+                //  repeat: Infinity,   // loop forever
+                // }}
+                // initial={{
+                //     y: "-50%"
+                // }}
+                //
+                // transition={{
+                //     duration: banner_animation_time,
+                //     repeat: Infinity
+                //
+                // }}
+                //
+                // animate={{
+                //     y: 0
+                // }}
                 ></motion.img>
-                <img src="../public/japan-bridge.png"
-                    className="absolute -top-70 bg-red-200 h-fit scale-90 "
-
-                ></img>
             </div>
 
             <Navbar></Navbar>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,6 +3,7 @@ import { motion } from "motion/react";
 import { useEffect } from "react";
 
 const Navbar = () => {
+
     useEffect(() => {
         return hover(".navTextLink", (element) => {
             const exitTransition = {
@@ -41,12 +42,12 @@ const Navbar = () => {
 
     return (
         <span className="sm:w-screen sm:flex sm:items-center sm:justify-center">
-            <nav className="w-screen sm:w-fit py-2 px-3 sm:p-5 xl:p-10 flex flex-wrap items-center gap-4 sm:gap-7 xl:gap-20">
+            <nav className="w-screen sm:w-fit py-4 px-3 sm:p-5 xl:p-10 flex flex-wrap items-center gap-4 sm:gap-7 xl:gap-20">
                 {["Home", "Blogs", "Projects", "About"].map((item) => (
                     <motion.a
                         key={item}
                         href={"/" + item.toLowerCase()}
-                        className="flex navTextLink items-center justify-between w-fit text-md md:text-2xl xl:text-5xl cursor-pointer"
+                        className="flex navTextLink items-center justify-between w-fit text-md md:text-xl xl:text-xl cursor-pointer"
                     >
                         <motion.div className="navTextContainer flex items-center gap-2 sm:gap-3 xl:gap-5">
                             <motion.div className="navTextLeftBracket w-fit">[</motion.div>
